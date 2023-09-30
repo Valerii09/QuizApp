8696dd68c258ba306701c150af6657c83de9fed4
Valerii09 <valiera.trufanov.2001@gmail.com>
2023-09-30 21:46:04 +0700

    изменен стиль

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.quizapp
 
 import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.RadioButton
@@ -10,6 +13,7 @@ import android.widget.RadioGroup
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     // Инициализируйте список вопросов
     private val questions = listOf(
@@ -32,7 +36,8 @@ class MainActivity : AppCompatActivity() {
                 AnswerOption("Argentina")
             ),
             1
-        ),
+       )
+        ,
         Question(
             "How many disciplines are there in men's gymnastics?",
             R.drawable.image3,
@@ -42,128 +47,129 @@ class MainActivity : AppCompatActivity() {
                 AnswerOption("Six")
             ),
             2
-        ),
-        Question(
-            "What is the record for the most red cards in one football match?",
-            R.drawable.image4,
-            listOf(
-                AnswerOption("36"),
-                AnswerOption("37"),
-                AnswerOption("38")
-            ),
-            0
-        ),
-        Question(
-            "Which African country was the first to qualify for the World Cup?",
-            R.drawable.image5,
-            listOf(
-                AnswerOption("Morocco"),
-                AnswerOption("Nigeria"),
-                AnswerOption("Egypt")
-            ),
-            2
-        ),
-        Question(
-            "Which city was the first to host the Olympics twice?",
-            R.drawable.image6,
-            listOf(
-                AnswerOption("Brasilia"),
-                AnswerOption("Paris"),
-                AnswerOption("Vancouver")
-            ),
-            1
-        ),
-        Question(
-            "What does NBA mean?",
-            R.drawable.image7,
-            listOf(
-                AnswerOption("National Running Association"),
-                AnswerOption("National Baseball Association"),
-                AnswerOption("National Basketball Association")
-            ),
-            2
-        ),
-        Question(
-            "Which part of the body in football cannot touch the ball?",
-            R.drawable.image8,
-            listOf(
-                AnswerOption("Knees"),
-                AnswerOption("Elbows"),
-                AnswerOption("Hands")
-            ),
-            2
-        ),
-        Question(
-            "In what year were women allowed to participate in the modern Olympic Games?",
-            R.drawable.image9,
-            listOf(
-                AnswerOption("1900"),
-                AnswerOption("1954"),
-                AnswerOption("1889")
-            ),
-            0
-        ),
-        Question(
-            "What is the only city in the United States that has won three of the four " +
-                    "major professional sports championships in the same year?",
-            R.drawable.image10,
-            listOf(
-                AnswerOption("Kentucky"),
-                AnswerOption("Iowa"),
-                AnswerOption("Detroit")
-            ),
-            2
-        ),
-        Question(
-            "Who is the youngest heavyweight boxing champion of the world?",
-                    R.drawable.image11,
-            listOf(
-                AnswerOption("Evander Holyfield"),
-                AnswerOption("Mike Tyson"),
-                AnswerOption("George Foreman")
-            ),
-            1
-        ),
-        Question(
-            "How many medals did China win at the Beijing Olympics?",
-                    R.drawable.image12,
-            listOf(
-                AnswerOption("100"),
-                AnswerOption("89"),
-                AnswerOption("96")
-            ),
-            0
-        ),
-        Question(
-            "How old was the youngest professional football player?",
-                    R.drawable.image13,
-            listOf(
-                AnswerOption("12"),
-                AnswerOption("18"),
-                AnswerOption("16")
-            ),
-            0
-        ),
-        Question(
-            "How many players are on a baseball team?",
-                    R.drawable.image14,
-            listOf(
-                AnswerOption("10"),
-                AnswerOption("9"),
-                AnswerOption("8")
-            ),
-            1
-        ),
-        Question(
-            "What was the fastest goal in the history of the World Cup?",
-                    R.drawable.image15,
-            listOf(
-                AnswerOption("9.8 seconds"),
-                AnswerOption("11.8 seconds"),
-                AnswerOption("10.8 seconds")
-            ),
-            2
         )
+        //,
+//        Question(
+//            "What is the record for the most red cards in one football match?",
+//            R.drawable.image4,
+//            listOf(
+//                AnswerOption("36"),
+//                AnswerOption("37"),
+//                AnswerOption("38")
+//            ),
+//            0
+//        ),
+//        Question(
+//            "Which African country was the first to qualify for the World Cup?",
+//            R.drawable.image5,
+//            listOf(
+//                AnswerOption("Morocco"),
+//                AnswerOption("Nigeria"),
+//                AnswerOption("Egypt")
+//            ),
+//            2
+//        ),
+//        Question(
+//            "Which city was the first to host the Olympics twice?",
+//            R.drawable.image6,
+//            listOf(
+//                AnswerOption("Brasilia"),
+//                AnswerOption("Paris"),
+//                AnswerOption("Vancouver")
+//            ),
+//            1
+//        ),
+//        Question(
+//            "What does NBA mean?",
+//            R.drawable.image7,
+//            listOf(
+//                AnswerOption("National Running Association"),
+//                AnswerOption("National Baseball Association"),
+//                AnswerOption("National Basketball Association")
+//            ),
+//            2
+//        ),
+//        Question(
+//            "Which part of the body in football cannot touch the ball?",
+//            R.drawable.image8,
+//            listOf(
+//                AnswerOption("Knees"),
+//                AnswerOption("Elbows"),
+//                AnswerOption("Hands")
+//            ),
+//            2
+//        ),
+//        Question(
+//            "In what year were women allowed to participate in the modern Olympic Games?",
+//            R.drawable.image9,
+//            listOf(
+//                AnswerOption("1900"),
+//                AnswerOption("1954"),
+//                AnswerOption("1889")
+//            ),
+//            0
+//        ),
+//        Question(
+//            "What is the only city in the United States that has won three of the four " +
+//                    "major professional sports championships in the same year?",
+//            R.drawable.image10,
+//            listOf(
+//                AnswerOption("Kentucky"),
+//                AnswerOption("Iowa"),
+//                AnswerOption("Detroit")
+//            ),
+//            2
+//        ),
+//        Question(
+//            "Who is the youngest heavyweight boxing champion of the world?",
+//                    R.drawable.image11,
+//            listOf(
+//                AnswerOption("Evander Holyfield"),
+//                AnswerOption("Mike Tyson"),
+//                AnswerOption("George Foreman")
+//            ),
+//            1
+//        ),
+//        Question(
+//            "How many medals did China win at the Beijing Olympics?",
+//                    R.drawable.image12,
+//            listOf(
+//                AnswerOption("100"),
+//                AnswerOption("89"),
+//                AnswerOption("96")
+//            ),
+//            0
+//        ),
+//        Question(
+//            "How old was the youngest professional football player?",
+//                    R.drawable.image13,
+//            listOf(
+//                AnswerOption("12"),
+//                AnswerOption("18"),
+//                AnswerOption("16")
+//            ),
+//            0
+//        ),
+//        Question(
+//            "How many players are on a baseball team?",
+//                    R.drawable.image14,
+//            listOf(
+//                AnswerOption("10"),
+//                AnswerOption("9"),
+//                AnswerOption("8")
+//            ),
+//            1
+//        ),
+//        Question(
+//            "What was the fastest goal in the history of the World Cup?",
+//                    R.drawable.image15,
+//            listOf(
+//                AnswerOption("9.8 seconds"),
+//                AnswerOption("11.8 seconds"),
+//                AnswerOption("10.8 seconds")
+//            ),
+//            2
+//        )
 
     )
     private var currentQuestionIndex = 0
@@ -243,22 +249,23 @@ class MainActivity : AppCompatActivity() {
             currentQuestionIndex++
             updateQuestionAndOptions()
         } else {
-            showResult()
+            showResultDialog()
         }
     }
 
-    private fun showResult() {
-        val resultText = "Your result: $score from ${questions.size}"
-        val alertDialog = AlertDialog.Builder(this)
-            .setTitle("Result")
-            .setMessage(resultText)
-            .setPositiveButton("Restart") { _, _ ->
-                restartQuiz()
-            }
-            .setCancelable(false)
-            .create()
-        alertDialog.show()
+    private fun showResultDialog() {
+        val resultIntent = Intent(this, ResultActivity::class.java)
+
+        // Добавьте количество правильных ответов в Intent
+        resultIntent.putExtra("score", score)
+
+        // Добавьте общее количество вопросов в Intent
+        resultIntent.putExtra("totalQuestions", questions.size)
+
+        // Перейдите к ResultActivity
+        startActivity(resultIntent)
     }
+
 
     private fun restartQuiz() {
         currentQuestionIndex = 0
---
+++ app/src/main/java/com/example/quizapp/ResultActivity.kt
@@ -0,0 +1,36 @@
+package com.example.quizapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        // Получите количество правильных ответов из Intent
+        val score = intent.getIntExtra("score", 0)
+        val totalQuestions = intent.getIntExtra("totalQuestions", 0)
+
+        // Далее, используйте значение score для отображения результата
+        val scoreTextView = findViewById<TextView>(R.id.scoreTextView)
+        scoreTextView.text = "Your score is $score out of 10"
+
+        val restartButton = findViewById<Button>(R.id.restartButton)
+
+
+        val scoreText = "Your score is $score out of $totalQuestions"
+        scoreTextView.text = scoreText
+
+        restartButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
